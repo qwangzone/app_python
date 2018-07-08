d94ccf6e159fa5c62ea0645087fc44fe898fa74c
578380132@qq.com <wq15803863660@>
2018-07-08 08:45:00 +0800

    Initial commit

---
+++ GouZao.py
@@ -0,0 +1,20 @@
+class GouZao:
+    b = 0
+
+    def __init__(self, a):
+        self.d = 1
+        self.b = a
+
+
+if __name__ == '__main__':
+    t1 = GouZao(1)
+    t2 = GouZao(2)
+    print(t1.b)
+    print(t2.d)
+
+
+
+
+
+
+
