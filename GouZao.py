1f7e92906af6e14d566c3c2043d52e26872ef239
578380132@qq.com <wq15803863660@>
2018-07-19 22:07:20 +0800

    add

--- GouZao.py
+++ GouZao.py
@@ -6,27 +6,36 @@ class GouZao:
         self.b = a
 
     def wq1(self):
-        print("wq1")
+        print("GouZao wq1")
 
     def wq2(self):
         print("wq2")
 
-class Wg:
+class Wg(GouZao):
     def __init__(self,shili):
         self.gouzao = shili
 
     def print1(self):
-        self.gouzao.wq1()
+        print("Wg1")
 
     def print2(self):
-        self.gouzao.wq2()
+        print("Wg2")
+    def wq1(self):
+        print("Wg wq1")
+
+    def test(self):
+        self.wq1()
+        super().wq1()
 
 
 if __name__ == '__main__':
     t1 = GouZao(1)
     t2 = GouZao(2)
     print(t1.b)
-    print(t2.d)
+    print(t2.b)
+    w = Wg(21)
+    w.print1()
+    w.test()
 
 
 
---
+++ args_test.py
@@ -0,0 +1,5 @@
+import sys
+a = sys.argv
+b="帅哥"
+print("wqqwq{name}".format("王奇"))
+#print(f"我是个大{b}") python 3.6引入
